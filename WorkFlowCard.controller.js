0a249e4e1b7bd8d654da3c5364889b6b148a8bcb
Pallavi141298 <100183971+Pallavi141298@users.noreply.github.com>
2022-04-25 15:20:42 +0530

    Add files via upload

---
+++ WorkFlowCard.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/integration/Host'
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.workflowDemo.WorkFlowCard.WorkFlowCard", {
+
+        onCardAction: function () {
+            debugger;
+            
+
+        }
+    });
+});
